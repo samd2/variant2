9f7a6b420093e330b8b55c4cf21e0d7e7b4565e2
sdarwin <samuel.d.darwin@gmail.com>
2020-12-23 16:15:21 +0000

    drone

---
+++ .drone/bdf8b0b5ef-863debdd74.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
+export VCS_COMMIT_ID=$DRONE_COMMIT
+export GIT_COMMIT=$DRONE_COMMIT
+export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> BEFORE_INSTALL'
+
+. .drone/before-install.sh
+
+echo '==================================> INSTALL'
+
+BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
+cd ..
+git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
+cd boost-root
+git submodule update --init tools/boostdep
+mkdir -p libs/variant2
+cp -r $TRAVIS_BUILD_DIR/* libs/variant2
+python tools/boostdep/depinst/depinst.py variant2
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> BEFORE_SCRIPT'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+
+echo '==================================> SCRIPT'
+
+pip install --user cmake
+mkdir __build__ && cd __build__
+cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBOOST_INCLUDE_LIBRARIES=variant2 -DCMAKE_INSTALL_PREFIX=~/.local ..
+cmake --build . --target install
+cd ../libs/variant2/test/cmake_install_test && mkdir __build__ && cd __build__
+cmake -DCMAKE_INSTALL_PREFIX=~/.local ..
+cmake --build .
+cmake --build . --target check
+
+echo '==================================> AFTER_SUCCESS'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
---
+++ .drone/boost.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
+export VCS_COMMIT_ID=$DRONE_COMMIT
+export GIT_COMMIT=$DRONE_COMMIT
+export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> BEFORE_INSTALL'
+
+. .drone/before-install.sh
+
+echo '==================================> INSTALL'
+
+BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
+cd ..
+git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
+cd boost-root
+git submodule update --init tools/boostdep
+mkdir -p libs/variant2
+cp -r $TRAVIS_BUILD_DIR/* libs/variant2
+python tools/boostdep/depinst/depinst.py variant2
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> BEFORE_SCRIPT'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+
+echo '==================================> SCRIPT'
+
+echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
+./b2 libs/variant2/test toolset=$TOOLSET cxxstd=$CXXSTD variant=debug,release ${UBSAN:+cxxflags=-fsanitize=undefined cxxflags=-fno-sanitize-recover=undefined linkflags=-fsanitize=undefined define=UBSAN=1 debug-symbols=on} ${LINKFLAGS:+linkflags=$LINKFLAGS}
+
+echo '==================================> AFTER_SUCCESS'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
